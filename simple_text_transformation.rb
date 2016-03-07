cf8d088bc3fc6e1f23815905a4a82571dfed8e65
Stephan Kämper <the.tester@seasidetesting.com>
2016-03-07 13:22:42 +0100

    Follow Ruby style guide (and RuboCop)

--- simple_text_transformation.rb
+++ simple_text_transformation.rb
@@ -3,6 +3,6 @@ require 'redcloth'
 filename = ARGV.first
 output_filename = File.basename(filename, '.*') + '.html'
 
-File.open(output_filename, 'w') do | f |
+File.open(output_filename, 'w') do |f|
   f.puts RedCloth.new(File.read(filename)).to_html
 end
