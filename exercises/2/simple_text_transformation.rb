163c7a2e6fe3fc4a99d5d67486aaebed5d552e85
Stephan Kämper <the.tester@seasidetesting.com>
2022-11-05 19:19:12 +0100

    Adds targetted file to exercise folder
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

---
+++ exercises/2/simple_text_transformation.rb
@@ -0,0 +1,8 @@
+require 'redcloth'
+
+filename = ARGV.first
+output_filename = File.basename(filename, '.*') + '.html'
+
+File.open(output_filename, 'w') do |f|
+  f.puts RedCloth.new(File.read(filename)).to_html
+end
